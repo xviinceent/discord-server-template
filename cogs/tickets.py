9750c6f73d00d06235956a2a6940554d93ae48ae
v <102491441+vxsualized@users.noreply.github.com>
2024-03-18 21:13:16 +0100

    ticket thing kinda finished

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,12 +1,15 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from views.close_ticket import CloseTicketView
  
 class Tickets(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    
+    @app_commands.command(name="close", description="Close a ticket")
+    async def close(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Are you sure you want to close this ticket?", view=CloseTicketView(), ephemeral=True)
  
 async def setup(bot: commands.Bot):
     await bot.add_cog(Tickets(bot))
---
+++ views/close_ticket.py
@@ -0,0 +1,48 @@
+import discord
+import aiosqlite
+import json
+import asyncio
+
+class CloseTicketView(discord.ui.View):
+    
+    @discord.ui.button(label="Close Ticket", style=discord.ButtonStyle.green, custom_id="button:close_ticket", emoji="❌")
+    async def close_ticket_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        conn = await aiosqlite.connect("database.db")
+        cur = await conn.cursor()
+        await cur.execute("SELECT USERID FROM tickets WHERE CHANNELID = ?", (interaction.channel.id,))
+        result = await cur.fetchone()
+        if not result:
+            await cur.close()
+            await conn.close()
+            await interaction.followup.send("❌ This is not a ticket channel.", ephemeral=True)
+            return
+        
+        with open("config.json", 'r') as f:
+            config = json.load(f)
+            try:
+                mod_role_id = config["moderator_role_id"]
+                admin_role_id = config["admin_role_id"]
+            except KeyError:
+                await cur.close()
+                await conn.close()
+                await interaction.followup.send("❌ Please check if all required config values are set. Please contact an admin.", ephemeral=True)
+                return
+        
+        mod_role = interaction.guild.get_role(mod_role_id)
+        admin_role = interaction.guild.get_role(admin_role_id)
+        checks = [mod_role in interaction.user.roles, admin_role in interaction.user.roles, interaction.user == interaction.guild.owner]
+
+        if any(checks) and result[0] != interaction.user.id:
+            await cur.close()
+            await conn.close()
+            await interaction.followup.send("❌ You do not have permission to close this ticket.", ephemeral=True)
+            return
+        
+        await cur.execute("DELETE FROM tickets WHERE CHANNELID = ?", (interaction.channel.id,))
+        await conn.commit()
+        await cur.close()
+        await conn.close()
+        await interaction.followup.send("✅ Ticket closed successfully. Channel will be deleted in 3 seconds.", ephemeral=True)
+        await asyncio.sleep(3)
+        await interaction.channel.delete()
