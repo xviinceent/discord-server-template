6d0a9c3f2dff341b514e93f1f7999318b5765fe2
v <102491441+vxsualized@users.noreply.github.com>
2024-03-22 16:04:47 +0100

    I HAVE NO IDEA WHAT I WROTE HERE

--- cogs/tempvoice.py
+++ cogs/tempvoice.py
@@ -17,16 +17,53 @@ class Tempvoice(commands.Cog):
                 tempvoice_creation_category_id = config["tempvoice_creation_category_id"]
             except KeyError:
                 return
-            
-        if before.channel is None and after.channel:
+        
+        if before.channel is None or not None and after.channel:
             tempvoice_creation_channel = member.guild.get_channel(tempvoice_creation_channel_id)
             if not tempvoice_creation_channel:
                 return
             if not after.channel == tempvoice_creation_channel:
+                tempvoice_creation_category = member.guild.get_channel(tempvoice_creation_category_id)
+                conn = await aiosqlite.connect("database.db")
+                cur = await conn.cursor()
+                await cur.execute("SELECT CHANNELID FROM tempvoice")
+                result = await cur.fetchall()
+                all_channels: list[int] = [row[0] for row in result]
+                await cur.close()
+                await conn.close()
+                reqs = [before.channel is not None, len(before.channel.members) <= 0, before.channel.category == tempvoice_creation_category, before.channel.id in all_channels]
+                if all(reqs):
+                    if after.channel == tempvoice_creation_channel:
+                        conn = await aiosqlite.connect("database.db")
+                        cur = await conn.cursor()
+                        await before.channel.delete()
+                        await cur.execute("DELETE FROM tempvoice")
+                        await conn.commit()
+                        await cur.close()
+                        await conn.close()
+                        return
+                    conn = await aiosqlite.connect("database.db")
+                    cur = await conn.cursor()
+                    await before.channel.delete()
+                    await cur.execute("DELETE FROM tempvoice")
+                    await conn.commit()
+                    await cur.close()
+                    await conn.close()
+                    return
                 return
             tempvoice_creation_category = member.guild.get_channel(tempvoice_creation_category_id)
             if not tempvoice_creation_category:
                 return
+            conn = await aiosqlite.connect("database.db")
+            cur = await conn.cursor()
+            await cur.execute("SELECT CHANNELID FROM tempvoice")
+            result = await cur.fetchall()
+            if result:
+                all_channels = [row[0] for row in result]
+                if before.channel.id in all_channels:
+                    await before.channel.delete()
+            await cur.close()
+            await conn.close()
             new_tempvoice = await tempvoice_creation_category.create_voice_channel(name=member.id)
             await member.move_to(new_tempvoice)
             conn = await aiosqlite.connect("database.db")
@@ -40,16 +77,31 @@ class Tempvoice(commands.Cog):
             await conn.close()
             return
         tempvoice_creation_category = member.guild.get_channel(tempvoice_creation_category_id)
-        reqs = [before.channel is not None, after.channel is None, len(before.channel.members) <= 0, before.channel.category == tempvoice_creation_category]
+        conn = await aiosqlite.connect("database.db")
+        cur = await conn.cursor()
+        await cur.execute("SELECT CHANNELID FROM tempvoice")
+        result = await cur.fetchall()
+        all_channels: list[int] = [row[0] for row in result]
+        await cur.close()
+        await conn.close()
+        reqs = [before.channel is not None, len(before.channel.members) <= 0, before.channel.category == tempvoice_creation_category, before.channel.id in all_channels]
         if all(reqs):
             conn = await aiosqlite.connect("database.db")
             cur = await conn.cursor()
             await before.channel.delete()
-            await cur.execute("DELETE FROM tempvoice WHERE OWNERID = ?", (member.id,))
+            await cur.execute("DELETE FROM tempvoice")
             await conn.commit()
             await cur.close()
             await conn.close()
             return
+
+    @app_commands.command(name="change-name", description="Change the name of your tempvoice channel")
+    async def change_name(self, interaction: discord.Interaction, name: str):
+        await interaction.response.send_message("this command will change your voice channels name, once implemented")
+    
+    @app_commands.command(name="max-users", description="Change max users in your tempvoice category")
+    async def max_users(self, interaction: discord.Interaction, max_users: int):
+        await interaction.response.send_message("this command will change the max users in your tempvoice category, once implemented")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Tempvoice(bot))
